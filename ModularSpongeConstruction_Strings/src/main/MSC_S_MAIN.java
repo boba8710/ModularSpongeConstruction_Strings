93a16c64d3001688a5f3525d74bcc8174bebf2d6
boba8710 <zgwwilson@gmail.com>
2018-06-22 01:11:19 -0400

    Added run termination conditions and handling
    A run now breaks smoothly on a Ctrl-C signal. Also will automatically end if the bitchange reaches a certain threshold. This is meant to prevent data loss when I'm constantly closing my laptop lid.

--- ModularSpongeConstruction_Strings/src/main/MSC_S_MAIN.java
+++ ModularSpongeConstruction_Strings/src/main/MSC_S_MAIN.java
@@ -9,41 +9,49 @@ import java.util.Scanner;
 
 public class MSC_S_MAIN {
 	public static void main(String[] args) {
-		
+		testFunction("NOT218,819#SW2232,396,426,590#SWA191,307,601,717#SW0304,519,846,1061#SW2622,642,715,735#SW044,276,44,276#XOR22,650,380,1008#XOR494,521,601,628#XOR421,631,454,664#LRO42,407,155#SW0521,656,527,662#LRO278,1047,183#SW0705,787,1189,1271#SW2664,709,1147,1192#SWA11,704,449,1142#LRO627,881,46#NOT2,991#SW0198,314,767,883#XOR233,711,580,1058#SW1430,503,1208,1281#SW0123,744,793,1414#LRO60,904,130#SW0393,466,878,951#SW1431,635,825,1029#XOR3,631,336,964#");
 		//CONFIGURATION
-		int popSize = 64;
-		int messageCount = 8192;
-		int messageLenBytes = 16;
-		int funcCount = 25;
-		int stateSize = 1600;
-		int rate = 300;
-		int capacity = 1600-rate;
-		double populationDieOffPercent = 0.50; //A higher value is more selective and less diverse, a lower value is the opposite
-		double mutationChance = 0.75;	//A higher value will increase the chance of random mutation in offspring
-		int preserveTopNIndividuals = 8;
-		int generationCount = 100;
+		int _popSize = 64;
+		int _funcCount = 25;
+		double _populationDieOffPercent = 0.50; //A higher value is more selective and less diverse, a lower value is the opposite
+		double _mutationChance = 0.75;	//A higher value will increase the chance of random mutation in offspring
+		int _preserveTopNIndividuals = 8;
+		int _generationCount = 100;
 		//adding -p will enable parameter entry
 		try {
 			if(args[0].equals("-p")) {
 				Scanner s = new Scanner(System.in);
 				System.out.println("Parameters:");
 				System.out.print("popSize=");
-				popSize = s.nextInt();
+				_popSize = s.nextInt();
 				System.out.print("\nfuncCount=");
-				funcCount = s.nextInt();
+				_funcCount = s.nextInt();
 				System.out.print("\npopulationDieOffPercent=");
-				populationDieOffPercent = s.nextDouble();
+				_populationDieOffPercent = s.nextDouble();
 				System.out.print("\nmutationChance=");
-				mutationChance = s.nextDouble();
+				_mutationChance = s.nextDouble();
 				System.out.print("\npreserveTopNIndividuals=");
-				preserveTopNIndividuals = s.nextInt();
+				_preserveTopNIndividuals = s.nextInt();
 				System.out.print("\ngenerationCount=");
-				generationCount = s.nextInt();
+				_generationCount = s.nextInt();
 				s.close();
 			}
 		}catch(Exception e) {
 			
 		}
+		final double bitchangeLowerBoundAutostop = 0.49;
+		final double bitchangeUpperBoundAutostop = 0.54;
+		final int popSize = _popSize;
+		final int messageCount = 8192;
+		final int messageLenBytes = 16;
+		final int funcCount = _funcCount;
+		final int stateSize = 1600;
+		final int rate = 300;
+		final int capacity = 1600-rate;
+		final double populationDieOffPercent = _populationDieOffPercent; //A higher value is more selective and less diverse, a lower value is the opposite
+		final double mutationChance = _mutationChance;	//A higher value will increase the chance of random mutation in offspring
+		final int preserveTopNIndividuals = _preserveTopNIndividuals;
+		final int generationCount = _generationCount;
 		
 		
 		//RANDOM GENERATION OF INITIAL POPULATION
@@ -51,6 +59,7 @@ public class MSC_S_MAIN {
 		String[] functionStringPop = new String[popSize];
 		ModularRoundFunction[] functionPop = new ModularRoundFunction[popSize];
 		SpongeConstruction_Strings[] spongeArray = new SpongeConstruction_Strings[popSize];
+		SpongeConstruction_Strings[] spongeArrayReserve = new SpongeConstruction_Strings[popSize];
 		String[] messages = new String[messageCount];
 		String[] messagesFlipped = new String[messageCount];
 		double[] scoreTable = new double[popSize];
@@ -73,9 +82,57 @@ public class MSC_S_MAIN {
 		
 		
 		
-		//Run GA
+		
 		Date runStart = new Date();
 		long runStartTime = runStart.getTime();
+		//Hook exit to runtime end
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				
+				Date runEnd = new Date();
+				try {
+					System.out.println("Run Ended.");
+					PrintWriter finalPopulationWriter = new PrintWriter("FinalRunPopulation"+runEnd.getTime()+".log");
+					PrintWriter dataWriter = new PrintWriter("RunData"+runEnd.getTime()+".csv");
+					finalPopulationWriter.println("Parameters:");
+					finalPopulationWriter.println("popSize: "+popSize);
+					finalPopulationWriter.println("messageCount: "+messageCount);
+					finalPopulationWriter.println("messageLength: "+messageLenBytes);
+					finalPopulationWriter.println("funcCount: "+funcCount);
+					finalPopulationWriter.println("stateSize: "+stateSize);
+					finalPopulationWriter.println("rate: "+rate);
+					finalPopulationWriter.println("capacity: "+capacity);
+					finalPopulationWriter.println("populationDieOffPercent: "+populationDieOffPercent);
+					finalPopulationWriter.println("mutationChance: "+mutationChance);
+					finalPopulationWriter.println("preserveTopNIndividuals: "+preserveTopNIndividuals);
+					finalPopulationWriter.println("generationCount: "+generationCount);
+					for(int i = 0; i < popSize; i++){
+						finalPopulationWriter.print(Integer.toString(i)+"	:\n");
+						finalPopulationWriter.print("Fitness Score:	"+spongeArrayReserve[i].geneticScore+"\n");
+						finalPopulationWriter.print("Bitchange:	"+(spongeArrayReserve[i].bitchangeScore)+"\n");
+						finalPopulationWriter.print("Round Function:\n");
+						finalPopulationWriter.print(spongeArrayReserve[i].f.getFunc()+"\n");
+						finalPopulationWriter.print("=================================================================\n\n");
+					}
+					for(int i = 0; i < generationCount; i++) {
+						dataWriter.print(i+","+topScores[i]+","+topBitchange[i]+"\n");
+					}
+					
+					System.out.println("File output succeeded, output saved to "+"FinalRunPopulation"+runEnd.getTime()+".log"+" and "+"RunData"+runEnd.getTime()+".csv");
+					finalPopulationWriter.close();
+					dataWriter.close();
+				} catch (IOException e1) {
+					System.out.println("File output failed.");
+					e1.printStackTrace();
+				}
+				
+				long runEndTime = runEnd.getTime();
+				System.out.println("Run completed. Run time elapsed: "+ghm.millisToTimestamp(runEndTime-runStartTime));
+			}
+		});
+
+		
+		//Run GA
 		for(int generation= 0; generation < generationCount-1; generation++) {
 			Date dateStart = new Date();
 			long startTime = dateStart.getTime();
@@ -106,9 +163,15 @@ public class MSC_S_MAIN {
 			topScores[generation] = spongeArray[popSize-1].geneticScore;
 			topBitchange[generation] = spongeArray[popSize-1].bitchangeScore;
 			System.out.println("Projected remaining runtime: "+ghm.millisToTimestamp((long)(endTime-startTime)*(generationCount-generation)));
+			for(int i = 0 ; i < popSize; i++) {
+				spongeArrayReserve[i] = spongeArray[i];
+			}
 			ghm.runGenerationOnSortedPopulation(spongeArray, populationDieOffPercent, mutationChance, preserveTopNIndividuals);
 			
-			
+			if(topBitchange[generation]<bitchangeUpperBoundAutostop&&topBitchange[generation]>bitchangeLowerBoundAutostop) {
+				System.out.println("Target bitchange detected! Autostop!");
+				break;
+			}
 			
 		}
 		
@@ -124,6 +187,7 @@ public class MSC_S_MAIN {
 				spongeArray[i].spongePurge();
 				score+=ghm.bitchange(h1,h2);
 			}
+			spongeArray[i].bitchangeScore = score/(double)messageCount;
 			spongeArray[i].geneticScore=1/Math.abs(0.5-(score/(double)messageCount));
 			scoreTable[i] = 1/Math.abs(0.5-(score/(double)messageCount));
 			System.out.printf("%.3f%s\n",(double)i*100/(double)popSize,"%");
@@ -134,47 +198,11 @@ public class MSC_S_MAIN {
 		System.out.println("Max bitchange:	"+(spongeArray[popSize-1].bitchangeScore));
 		topScores[generationCount-1] = spongeArray[popSize-1].geneticScore;
 		topBitchange[generationCount-1] = spongeArray[popSize-1].bitchangeScore;
-		
-		
-		//Output run data
-		Date runEnd = new Date();
-		try {
-			PrintWriter finalPopulationWriter = new PrintWriter("FinalRunPopulation"+runEnd.getTime()+".log");
-			PrintWriter dataWriter = new PrintWriter("RunData"+runEnd.getTime()+".csv");
-			finalPopulationWriter.println("Parameters:");
-			finalPopulationWriter.println("popSize: "+popSize);
-			finalPopulationWriter.println("messageCount: "+messageCount);
-			finalPopulationWriter.println("messageLength: "+messageLenBytes);
-			finalPopulationWriter.println("funcCount: "+funcCount);
-			finalPopulationWriter.println("stateSize: "+stateSize);
-			finalPopulationWriter.println("rate: "+rate);
-			finalPopulationWriter.println("capacity: "+capacity);
-			finalPopulationWriter.println("populationDieOffPercent: "+populationDieOffPercent);
-			finalPopulationWriter.println("mutationChance: "+mutationChance);
-			finalPopulationWriter.println("preserveTopNIndividuals: "+preserveTopNIndividuals);
-			finalPopulationWriter.println("generationCount: "+generationCount);
-			for(int i = 0; i < popSize; i++){
-				finalPopulationWriter.print(Integer.toString(i)+"	:\n");
-				finalPopulationWriter.print("Fitness Score:	"+spongeArray[i].geneticScore+"\n");
-				finalPopulationWriter.print("Bitchange:	"+(spongeArray[i].bitchangeScore));
-				finalPopulationWriter.print("Round Function:\n");
-				finalPopulationWriter.print(spongeArray[i].f.getFunc()+"\n");
-				finalPopulationWriter.print("=================================================================\n\n");
-			}
-			for(int i = 0; i < generationCount; i++) {
-				dataWriter.print(i+","+topScores[i]+","+topBitchange[i]+"\n");
-			}
-			
-			System.out.println("File output succeeded, output saved to "+"FinalRunPopulation"+runEnd.getTime()+".log"+" and "+"RunData"+runEnd.getTime()+".csv");
-			finalPopulationWriter.close();
-			dataWriter.close();
-		} catch (IOException e1) {
-			System.out.println("File output failed.");
-			e1.printStackTrace();
+		System.out.println(topBitchange[generationCount-1]);
+		for(int i = 0 ; i < popSize; i++) {
+			spongeArrayReserve[i] = spongeArray[i];
 		}
 		
-		long runEndTime = runEnd.getTime();
-		System.out.println("Run completed. Run time elapsed: "+ghm.millisToTimestamp(runEndTime-runStartTime));
 		
 		
 		
@@ -204,5 +232,32 @@ public class MSC_S_MAIN {
 		Arrays.sort(scoreTable);
 		System.out.println("Max of run: "+scoreTable[scoreTable.length-1]);
 		*/
+	}
+	static void testFunction(String function) {
+		SpongeConstruction_Strings testingFunc = new SpongeConstruction_Strings(1600, 300, 1300, new ModularRoundFunction(1600, function));
+		GeneticHelperMethods ghm = new GeneticHelperMethods();
+		String[] messages = new String[8196];
+		String[] messagesFlipped = new String[8196];
+		
+		for(int i = 0; i < messages.length; i++) {
+			messages[i]=ghm.generateMersenneRandomString(16);
+			messagesFlipped[i]=ghm.flipRand(messages[i]);
+		}
+		
+			double score = 0;
+			for(int j = 0; j < messages.length; j++) {
+				testingFunc.spongeAbsorb(messages[j]);
+				String h1 = testingFunc.spongeSqueeze(1);
+				testingFunc.spongePurge();
+				testingFunc.spongeAbsorb(messagesFlipped[j]);
+				String h2 = testingFunc.spongeSqueeze(1);
+				testingFunc.spongePurge();
+				score+=ghm.bitchange(h1,h2);
+			}
+			testingFunc.bitchangeScore = score/(double)8196;
+			testingFunc.geneticScore=1/Math.abs(0.5-(score/(double)8196));
+			System.out.println("Function under test scored a bitchange of: "+testingFunc.bitchangeScore);
+			System.out.println("And a cooresponding fitness score of     : "+testingFunc.geneticScore);
+		
 	}
 }
