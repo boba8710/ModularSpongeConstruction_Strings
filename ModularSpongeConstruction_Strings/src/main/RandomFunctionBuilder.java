b2d2871459438833fb8aa0cef66d2813774fbef9
Zaine Wilson <zgwwilson@gmail.com>
2018-07-16 23:35:32 -0400

    Changed EVERYTHING to 64 bit words
    Lots changed in the random function builder!

--- ModularSpongeConstruction_Strings/src/main/RandomFunctionBuilder.java
+++ ModularSpongeConstruction_Strings/src/main/RandomFunctionBuilder.java
@@ -22,49 +22,40 @@ public class RandomFunctionBuilder {
 			
 		}else if(selected.getId() == "ADD"){
 			int n,m;
-			while(true) {
-				n = rand.nextInt(200)*8;
-				m = rand.nextInt(200)*8;
-				if(n<m) {
-					break;
-				}
-			}
-			int pInt = rand.nextInt((int) Math.pow(2, m-n));
-			String p = Integer.toBinaryString(pInt);
+
+			n = rand.nextInt(25)*64;
+			m = n+64;
+			
+
+			String p = ghm.generateMersenneRandomString(64);
 			paramString+=n+","+m+","+p;
 		}else if(selected.getId() == "LRO" || selected.getId() == "NOT") {
 			int r, s;
-			while(true) {
-				r = rand.nextInt(200)*8;
-				s = rand.nextInt(200)*8+8;
-				if(s>r) {
-					break;
-				}
+
+			r = rand.nextInt(25)*64;
+			s = r+64;
+
+			paramString+=r+","+s;
+			if(selected.getId() == "LRO") {
+				paramString+=","+rand.nextInt(64);
 			}
-				paramString+=r+","+s;
-				if(selected.getId() == "LRO") {
-					paramString+=","+rand.nextInt(s-r);
-				}
 			}else if(selected.getId() == "XOC"){ 
 				int n,m;
-				n = rand.nextInt(192)*8;
-				m = n+(rand.nextInt(7)+1)*8+8;
-				String p = ghm.generateMersenneRandomString((m-n)/8);
+				n = rand.nextInt(25)*64;
+				m = n+64;
+				String p = ghm.generateMersenneRandomString(64);
 				paramString+=n+","+m+","+p;
 			}else{	 
 				int n,m;
-				int offset = rand.nextInt(100)*8; 
-				while(true) {
-					n = rand.nextInt(100)*8;
-					m = (rand.nextInt(100)*8)+8;
-					if(n<m) {
-						break;
-					}
-				}
+				int offset = rand.nextInt(14)*64; 
+
+				n = rand.nextInt(13)*64;
+				m = n+64;
+
 				int p = n+offset;
 				int q = m+offset;
 				paramString+=n+","+m+","+p+","+q;
-		}
+			}
 		//No hash here, the GenRandOperation works on elements of arrays already delimited with hashes
 		return paramString;
 		
@@ -76,58 +67,44 @@ public class RandomFunctionBuilder {
 		for(int functionCount = 0; functionCount < funcCount; functionCount++) { //This loop runs until we reach functionCount of functions
 			HashOperation selected = operations[rand.nextInt(operations.length)]; //select a random operation from our operations array (static)
 			String paramString = selected.getId(); //start the paramString (string storing one operation) with the three letter identifier
-			if(selected.getId() == "NOP"){ //If it's a NOP, we don't have to do anything
-			}else if(selected.getId() == "ADD"){ //ADD requires a n and m (the start and end of a state chunk) and a p, which is the random integer to add to
-												 //the chunk
+			if(selected.getId() == "NOP"){
+				
+			}else if(selected.getId() == "ADD"){
 				int n,m;
-				while(true) { //This process is slightly inefficient, involving some wasted cycles when m>n. This, however, should only be 50% of cycles.
-					n = rand.nextInt(200)*8;
-					m = rand.nextInt(200)*8+8;
-					if(n<m) {
-						break;
-					}
-				}
-				int pInt = rand.nextInt((int) Math.pow(2, m-n)); //This /should/ ensure the random integer's bitstring isn't longer than the chunk
-				String p = Integer.toBinaryString(pInt);//convert the integer to a bitstring
-				paramString+=n+","+m+","+p; //assemble the operation
-			}else if(selected.getId() == "LRO" || selected.getId() == "NOT") { //LRO and NOT both act on one hash chunk. 
+
+				n = rand.nextInt(25)*64;
+				m = n+64;
+				
+
+				String p = ghm.generateMersenneRandomString(64);
+				paramString+=n+","+m+","+p;
+			}else if(selected.getId() == "LRO" || selected.getId() == "NOT") {
 				int r, s;
-				while(true) {
-					r = rand.nextInt(200)*8;
-					s = rand.nextInt(200)*8+8;
-					if(s>r) {
-						break;
-					}
+
+				r = rand.nextInt(25)*64;
+				s = r+64;
+
+				paramString+=r+","+s;
+				if(selected.getId() == "LRO") {
+					paramString+=","+rand.nextInt(64);
 				}
-					paramString+=r+","+s;
-					if(selected.getId() == "LRO") {
-						paramString+=","+rand.nextInt(s-r); //Generate a rotation constant no larger than the length in bits of the current bitstring
-					}
-				}else if(selected.getId() == "XOC"){//This operation exclusive ors a round constant into a section of the state.
+				}else if(selected.getId() == "XOC"){ 
 					int n,m;
-					n = rand.nextInt(192)*8;
-					m = n+(rand.nextInt(7)+1)*8+8;
-					String p = ghm.generateMersenneRandomString((m-n)/8);
+					n = rand.nextInt(25)*64;
+					m = n+64;
+					String p = ghm.generateMersenneRandomString(64);
 					paramString+=n+","+m+","+p;
-				}else{//This serves as a catchall for the other operations, OR, AND, and XOR. 
-					  //The general form of these operations is:
-					  //Select an offset between 0-799 (an offset of zero is almost always not condusive to good behavior, but it is left in for completeness)
-					  //Generate upper and lower bounds that are in the FIRST HALF of the state
-					  //adding the offset to the upper and lower bounds then creates a second set of bounds, equivalent in length to the first
-					  //The operation will be applied in the form (firstChunk * secondChunk), where '*' represents a bitwise operation.
+				}else{	 
 					int n,m;
-					int offset = rand.nextInt(100)*8;
-					while(true) {
-						n = rand.nextInt(100)*8;
-						m = (rand.nextInt(100)*8)+8;
-						if(n<m) {
-							break;
-						}
-					}
+					int offset = rand.nextInt(14)*64; 
+
+					n = rand.nextInt(13)*64;
+					m = n+64;
+
 					int p = n+offset;
 					int q = m+offset;
 					paramString+=n+","+m+","+p+","+q;
-			}
+				}
 			paramString+="#"; //Here's the hash delimiter; it signifies the end of a single operation
 			retString+=paramString;
 		}
